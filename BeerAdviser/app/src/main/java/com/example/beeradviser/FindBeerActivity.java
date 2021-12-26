7638355159ce720ef478e2abe20ac127c6bdc31d
masud563 <sydmasud563@gmail.com>
2021-12-26 23:05:33 +0600

    done

---
+++ BeerAdviser/app/src/main/java/com/example/beeradviser/FindBeerActivity.java
@@ -0,0 +1,20 @@
+package com.example.beeradviser;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+public class FindBeerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+    public void onClickFindBeer(View view){
+
+        TextView brands = (Textview) findViewById(R.id.)
+    }
+}
